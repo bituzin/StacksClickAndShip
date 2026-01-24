a913fd0998045609cf304e44f45428bc4fedd588
bituzin <bituzin2@gmail.com>
2026-01-24 16:13:53 +0100

    [UI] Shorten description in 'Get Your Name' card to one line

--- src/components/StacksClickAndShip.tsx
+++ src/components/StacksClickAndShip.tsx
@@ -717,7 +717,7 @@ export default function StacksClickAndShip({
                 <Link to="/getname" className="bg-orange-900/40 rounded-xl p-6 border border-orange-500/20 hover:border-orange-400/50 transition-all cursor-pointer" style={{ textDecoration: 'none' }}>
                   <User className="text-yellow-400 mb-3" size={32} />
                   <h3 className="text-xl text-white mb-2">Get Your Name</h3>
-                  <p className="text-orange-300">Register your unique username on Stacks blockchain</p>
+                  <p className="text-orange-300">Register your unique Stacks username</p>
                 </Link>
 
                 <Link to="/learn" className="bg-orange-900/40 rounded-xl p-6 border border-orange-500/20 hover:border-orange-400/50 transition-all cursor-pointer" style={{ textDecoration: 'none' }}>
