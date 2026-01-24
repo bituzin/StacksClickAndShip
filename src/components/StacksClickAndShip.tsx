f0fce5e40a7350615648ae70ab68b8dd8a58d2db
bituzin <bituzin2@gmail.com>
2026-01-24 16:10:22 +0100

    [UI] Unify icon color in 'Get Your Name' card on home

--- src/components/StacksClickAndShip.tsx
+++ src/components/StacksClickAndShip.tsx
@@ -721,7 +721,7 @@ export default function StacksClickAndShip({
                 </Link>
 
                 <Link to="/getname" className="bg-orange-900/40 rounded-xl p-6 border border-orange-500/20 hover:border-orange-400/50 transition-all cursor-pointer" style={{ textDecoration: 'none' }}>
-                  <User className="text-cyan-600 mb-3" size={32} />
+                  <User className="text-orange-400 mb-3" size={32} />
                   <h3 className="text-xl text-white mb-2">Get Your Name</h3>
                   <p className="text-orange-300">Register your unique username on Stacks blockchain</p>
                 </Link>
