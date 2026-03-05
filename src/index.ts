a15ddde36de9878326debaa65a0fb987a9e2857f
bituzin <bituzin2@gmail.com>
2026-03-05 22:01:08 +0100

    Feat: add npm library build config (vite.lib.config.ts, src/index.ts, .npmignore)

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { default as StacksClickAndShip } from './components/StacksClickAndShip';
+export { default as SayGMCard } from './components/SayGMCard';
+export { default as PostMessageCard } from './components/PostMessageCard';
+export { default as VoteCard } from './components/VoteCard';
+export { default as GetNameCard } from './components/GetNameCard';
+export { default as LearnCard } from './components/LearnCard';
+export { default as MyStatsCard } from './components/MyStatsCard';
+export * from './hooks';
+export * from './types';
